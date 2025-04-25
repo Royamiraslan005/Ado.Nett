a584b7654222677617b901627523e8db9108ac1a
Royamiraslan005 <royaca-ab109@code.edu.az>
2025-04-25 04:12:18 +0400

    kodlar duzeldi ve yenilendi

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -8,5 +8,27 @@ namespace ConsoleApp28.Context
 {
     class AppDbContext
     {
+        const string con = " server=DESKTOP-O23G9HC ; database= AB109_Ado;Trusted_connection=true";
+        SqlConnection sqlConnection = new SqlConnection(con);
+        public int ExecuteNonQuery(string command)
+        {
+
+            sqlConnection.Open();
+            SqlCommand sql = new SqlCommand(command, sqlConnection);
+            int result = sql.ExecuteNonQuery();
+            sqlConnection.Close();
+            return result;
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            sqlConnection.Open();
+            SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(query, sqlConnection);
+            DataTable dataTable = new DataTable();
+            sqlDataAdapter.Fill(dataTable);
+            sqlConnection.Close();
+            return dataTable;
+        }
+
     }
+}
 }
--- Models/USER.cs
+++ Models/USER.cs
@@ -8,5 +8,8 @@ namespace ConsoleApp28.Models
 {
     class USER
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
     }
 }
